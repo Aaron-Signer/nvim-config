cf20fb5bdeaf7974d6b9eea0b60ffe59c81a9137
Aaron Signer <aaronsigner1995@proton.me>
2025-06-24 21:18:53 -0400

    Added support for python LSP and autocomplete. Need to improve

--- init.lua
+++ init.lua
@@ -2,4 +2,47 @@ require("config.lazy")
 require("config.remap")
 require("config.set")
 vim.lsp.enable('lua_ls')
-vim.lsp.enable('pylsp')
+vim.lsp.enable('pyright')
+
+local function git_branch()
+    local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
+    if string.len(branch) > 0 then
+        return branch
+    else
+        return ":"
+    end
+end
+
+local function statusline()
+    local set_color_1 = "%#PmenuSel#"
+    local branch = git_branch()
+    local set_color_2 = "%#LineNr#"
+    local file_name = " %f"
+    local modified = "%m"
+    local align_right = "%="
+    local fileencoding = " %{&fileencoding?&fileencoding:&encoding}"
+    local fileformat = " [%{&fileformat}]"
+    local filetype = " %y"
+    local percentage = " %p%%"
+    local linecol = " %l:%c"
+	local total_lines = "%{line('$')}"
+
+    return string.format(
+        "%s %s %s%s%s%s%s%s%s%s%s | %s",
+        set_color_1,
+        branch,
+        set_color_2,
+        file_name,
+        modified,
+        align_right,
+        filetype,
+        fileencoding,
+        fileformat,
+        percentage,
+        linecol,
+	total_lines
+    )
+end
+
+vim.opt.statusline = statusline()
+
