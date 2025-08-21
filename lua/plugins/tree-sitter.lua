528be103e6bc9ff381a85a1e5ef5c1cbab68512e
Aaron Signer <aaronsigner1995@proton.me>
2025-08-21 15:55:47 -0400

    WIP

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,19 +1,20 @@
-return {
-  "nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	branch = "main",
-build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-	configs.setup({
-		ensure_installed = {
-			"lua", "python"
-	},
+--return {
+--  "nvim-treesitter/nvim-treesitter",
+--	lazy = false,
+--	branch = "main",
+--build = ":TSUpdate",
+--	config = function()
+--		local configs = require("nvim-treesitter.configs")
+--
+--
+--	configs.setup({
+--		ensure_installed = {
+--			"lua", "python"
+--	},
 --	auto_install = true,
-	sync_install = false,
-	highlight = { enable = true},
-	indent = {enable = true},
-	})
-	end
-}
+--	sync_install = false,
+--	highlight = { enable = true},
+--	indent = {enable = true},
+--	})
+--	end
+--}
